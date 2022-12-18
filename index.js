2726755cc46c8cabeede2c7a2fd349090e715652
DylanRhinehart <dylanrhinehart@gmail.com>
2022-12-18 15:25:06 -0800

    commit tree d379c227eb9fe03761832c8f753da11f7db8d5e8

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+const body = document.querySelector('body');
+
+function submitData(userName, userEmail){
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: `${userName}`,
+            email: `${userEmail}`
+        })
+})
+   .then((response) => {return (response.json())})
+   .then((object) => {
+    body.append(object.id)
+    body.append(object.name)
+    body.append(object.email)
+})
+   .catch((error) => {body.append(error.message)}); 
+}
+
+submitData("Dylan","test@gmail.com");
